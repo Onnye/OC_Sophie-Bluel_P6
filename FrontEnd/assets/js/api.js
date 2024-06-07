091e6f4177d68ab412dbdb7abb49d73720dd6aaf
Laura TUFO <laura.tufo94@gmail.com>
2024-06-07 17:21:01 +0200

    Modification - function fetch - add option for HTTP request

--- FrontEnd/assets/js/api.js
+++ FrontEnd/assets/js/api.js
@@ -1,15 +1,16 @@
 import { host } from "./config.js";
 
-// Fonction utilitaire pour les requêtes fetch.
-export async function fetchData(endpoint) {
+// Fonction utilitaire pour les requêtes fetch
+export async function fetchApi(endpoint, options = {}) {
+  const url = `${host}${endpoint}`;
   try {
-    const response = await fetch(`${host}${endpoint}`);
+    const response = await fetch(url, options);
     if (!response.ok) {
       throw new Error(
         `La requête a échoué avec le statut : ${response.status}`
       );
     }
-    return response.json();
+    return await response.json();
   } catch (error) {
     console.error(
       `Erreur lors de la récupération des données depuis ${endpoint}:`,
