19fb37b4c97d38fd9bd090ff1b42e27de1dc4a75
Laura TUFO <laura.tufo94@gmail.com>
2024-06-08 18:49:19 +0200

    Add function for edit banner - Admin mode + modifications HTML and CSS

--- FrontEnd/assets/js/auth.js
+++ FrontEnd/assets/js/auth.js
@@ -16,6 +16,27 @@ export function manageAuthLinks() {
   }
 }
 
+// Fonction pour afficher la bannière si le token est valide
+export function showBannerIfValidToken() {
+  const banner = document.getElementById("editBanner");
+  const iconModify = document.querySelector(".icon-banner");
+  const userToken = localStorage.getItem("token");
+
+  // Log pour vérifier la récupération du token
+  console.log("Token récupéré :", userToken);
+
+  if (userToken) {
+    // Affiche la bannière et les éléments administrateur
+    if (banner) banner.style.display = "block";
+    if (iconModify) iconModify.style.display = "block";
+  } else {
+    // Cache la bannière et les éléments administrateur
+    if (banner) banner.style.display = "none";
+    if (iconModify) iconModify.style.display = "none";
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   manageAuthLinks();
+  showBannerIfValidToken();
 });
